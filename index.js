b6220828917d5fb49eefc5e369cb2a808a663da9
ayrtonftridico <ayrtonftridico@gmail.com>
2022-04-01 15:58:50 -0300

    1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();                    // inicialização do express
+require('dotenv').config()
 
 const Person = require ('./models/Person')
 
@@ -12,25 +13,33 @@ app.use(                                  // middleware de ler JSON
 
 app.use(express.json());
 
-app.post('/person', async (req, res) =>{
+app.post('/person', async (req, res) => {
 
-    const {name, salary, approved} = req.body
+    const {name, salary, approved} = req.body;
+
+    if (!name)
+    {
+        res.status(422).json({ error: 'O nome é obrigatório!! '})
+    }
 
     const person = {
         name,
         salary,
         approved
-    }
+    };
 
     try {
-        await Person.create();
+
+        await Person.create(person);
+        res.status(201).json({message: 'Pessoa inserida no sistema com sucesso'})
+
     } catch (error) {
         res.status(500).json({error: error})
-    }
+    };
 });
 
-const DB_USER= 'ayrtonftridico'
-const DB_PASS= 'DDJElvvgz8FwN7pN'
+const DB_USER= process.env.DBUSER
+const DB_PASS= process.env.DBPASS
 
 mongoose
     .connect(`mongodb+srv://${DB_USER}:${DB_PASS}@apicluster.ahxxt.mongodb.net/bdapi?retryWrites=true&w=majority`
